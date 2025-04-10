54904b6296742f26207fb285ea93293051b66a88
coolshou <jimmy_yeh@alphanetworks.com>
2025-04-10 14:23:30 +0800

    fix qtenv path

--- build_x64.bat
+++ build_x64.bat
@@ -9,13 +9,15 @@ if !errorlevel! neq 0 (
     set "driveLetter=%currentDir:~0,2%"
     echo set QT msvc2022_64 PATH
     REM this will enter another comspec!
-    %comspec% /k "C:\Qt\6.8.0\msvc2022_64\bin\qtenv2.bat"
+    echo "C:\Qt\6.8.3\msvc2022_64\bin\qtenv2.bat"
+    "C:\Qt\6.8.3\msvc2022_64\bin\qtenv2.bat"
     %driveLetter%
 )
 echo "Check Visual Studio 2022 build environment"
 echo "%PATH%" | findstr /c:"Visual Studio"
 if !errorlevel! neq 0 (
     echo set Visual Studio 2022 Community PATH
+    echo %comspec% /k "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
     %comspec% /k "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 )
 
@@ -23,4 +25,5 @@ echo qmake...
 qmake
 echo nmake...
 nmake
+echo multiftp_x86_64\multiftp.exe
 windeployqt multiftp_x86_64\multiftp.exe

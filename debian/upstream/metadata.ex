a11509b26e9a56cd9032fc0667b3afe4b58e2901
coolshou <jimmy_yeh@alphanetworks.com>
2025-04-10 15:35:00 +0800

    add debian files

---
+++ debian/upstream/metadata.ex
@@ -0,0 +1,10 @@
+# Example file for upstream/metadata.
+# See https://wiki.debian.org/UpstreamMetadata for more info/fields.
+# Below an example based on a github project.
+
+# Bug-Database: https://github.com/<user>/multiftp/issues
+# Bug-Submit: https://github.com/<user>/multiftp/issues/new
+# Changelog: https://github.com/<user>/multiftp/blob/master/CHANGES
+# Documentation: https://github.com/<user>/multiftp/wiki
+# Repository-Browse: https://github.com/<user>/multiftp
+# Repository: https://github.com/<user>/multiftp.git
